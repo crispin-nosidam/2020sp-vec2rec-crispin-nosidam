888b1bddcc63b41aeb25eae8853e68344a0fb588
crispin-nosidam <60271416+crispin-nosidam@users.noreply.github.com>
2020-04-28 22:02:28 +0800

    feat(preprocessing): include preprocessing tools and samples to compile jobs desc, resumes and training desc

---
+++ vec2rec/preprocess/tools.py
@@ -0,0 +1,44 @@
+import nltk
+import os
+import pandas as pd
+import re
+from glob import glob
+from nltk.corpus import stopwords
+from krovetzstemmer import Stemmer
+from PyPDF2 import PdfFileReader
+
+
+def extract_pdf_text(path, format="string"):
+    with open(path, "rb") as fileobj:
+        pfr = PdfFileReader(fileobj)
+        text = "" if format == "string" else []
+        for pg in range(pfr.getNumPages()):
+            if format == "string":
+                text += pfr.getPage(pg).extractText()
+            else:
+                text.append(pfr.getPage(pg).extractText())
+        return text
+
+
+def tokenize(text):
+    stemmer = Stemmer()
+    return [
+        stemmer.stem(token.lower())
+        for token in nltk.word_tokenize(re.sub("\n", "", text))
+        if (token.isalnum() and token.lower() not in stopwords.words("english"))
+    ]
+
+
+def pdf_to_dataframe(dir, file_glob="*.pdf"):
+    df = pd.DataFrame(columns=["filename", "tokens", "length"]).astype({"length": "int"})
+    for file in glob(os.path.join(dir, file_glob)):
+        tokens = tokenize(extract_pdf_text(os.path.join(dir, file)))
+        df = df.append(
+            {
+                "filename": os.path.basename(file),
+                "tokens": tokens,
+                "length": len(tokens),
+            },
+            ignore_index=True,
+        )
+    return df
