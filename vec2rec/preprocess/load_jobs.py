669b184f2a7faab7c1375a1ab4538ff4dfe95ee5
crispin-nosidam <60271416+crispin-nosidam@users.noreply.github.com>
2020-04-30 19:07:13 +0800

    refactor(job): change expected job desc column name from excel

--- vec2rec/preprocess/load_jobs.py
+++ vec2rec/preprocess/load_jobs.py
@@ -6,7 +6,7 @@ DATA_DIR = os.path.abspath("vec2rec/data/job")
 FILE_NAME = "Updated-Job-Descriptions-2018 (edited).xlsx"
 
 df = pd.read_excel(os.path.join(DATA_DIR, FILE_NAME))
-df["Job Description"] = df["Job Description"].apply(tokenize)
+df["Description"] = df["Description"].apply(tokenize)
 pd.set_option('display.max_columns', None)
 pd.set_option("max_colwidth", 30)
 print(df)
