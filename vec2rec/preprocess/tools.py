0176fbb877602b655d9556a7be7c943b055bcf4a
crispin-nosidam <60271416+crispin-nosidam@users.noreply.github.com>
2020-05-02 22:56:56 +0800

    refactor(Tokenizer, PDFReader): refactor as descriptor classes

--- vec2rec/preprocess/tools.py
+++ vec2rec/preprocess/tools.py
@@ -13,38 +13,16 @@ from string import punctuation
 from PyPDF4 import PdfFileReader
 
 
-class TokenData:
-    df_schema = ["doc_type", "filename", "ID", "tokens", "length"]
-    default_fp = {
-        "job": "job.parquet",
-        "resume": "resume.parquet",
-        "train": "train.parquet",
-    }
-    res_df = dd.from_pandas(
-        pd.DataFrame(columns=df_schema).astype({"length": "int"}), chunksize=20
-    )
-    job_df = dd.from_pandas(
-        pd.DataFrame(columns=df_schema).astype({"length": "int"}), chunksize=20
-    )
-    train_df = dd.from_pandas(
-        pd.DataFrame(columns=df_schema).astype({"length": "int"}), chunksize=20
-    )
+class Tokenizer:
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance
+
+    def __call__(self, text):
+        return self.tokenize(text)
 
     @staticmethod
-    @dask.delayed
-    def extract_pdf_text(path, format="string"):
-        with open(path, "rb") as fileobj:
-            pfr = PdfFileReader(fileobj, strict=False)
-            text = "" if format == "string" else []
-            for pg in range(pfr.getNumPages()):
-                if format == "string":
-                    text += pfr.getPage(pg).extractText()
-                else:
-                    text.append(pfr.getPage(pg).extractText())
-            return text
-
-    @staticmethod
-    @dask.delayed
     def tokenize(text):
         stemmer = Stemmer()
         return [
@@ -63,21 +41,75 @@ class TokenData:
             )
         ]
 
+
+class PDFReader:
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance
+
+    def __call__(self, *args, **kwargs):
+        return self.extract_pdf_text(*args, **kwargs)
+
+    @staticmethod
+    def extract_pdf_text(path, fmt="string"):
+        with open(path, "rb") as fileobj:
+            pfr = PdfFileReader(fileobj, strict=False)
+            text = "" if fmt == "string" else []
+            for pg in range(pfr.getNumPages()):
+                if fmt == "string":
+                    text += pfr.getPage(pg).extractText()
+                else:
+                    text.append(pfr.getPage(pg).extractText())
+            return text
+
+
+class TokenData:
+    df_schema = ["doc_type", "filename", "ID", "tokens", "length"]
+    default_fp = {
+        "job": "job.parquet",
+        "resume": "resume.parquet",
+        "train": "train.parquet",
+    }
+
+    def __init__(self, chunk_size=20):
+        self.chunk_size = chunk_size
+        self.res_df = dd.from_pandas(
+            pd.DataFrame(columns=self.df_schema).astype({"length": "int"}),
+            chunksize=chunk_size,
+        )
+        self.job_df = dd.from_pandas(
+            pd.DataFrame(columns=self.df_schema).astype({"length": "int"}),
+            chunksize=chunk_size,
+        )
+        self.train_df = dd.from_pandas(
+            pd.DataFrame(columns=self.df_schema).astype({"length": "int"}),
+            chunksize=chunk_size,
+        )
+
+    extract_pdf_text = staticmethod(dask.delayed(PDFReader()))
+
+    tokenize = staticmethod(dask.delayed(Tokenizer()))
+
     def pdf_to_df(self, parent_dir, file_glob="*.pdf", df_type="resume"):
         df = pd.DataFrame(columns=self.df_schema).astype({"length": "int"})
-        df = dd.from_pandas(df, chunksize=20)
+        df = dd.from_pandas(df, chunksize=self.chunk_size)
         for file in glob(os.path.join(parent_dir, file_glob)):
             tokens = self.tokenize(
                 self.extract_pdf_text(os.path.join(parent_dir, file))
             )
-            df_part = pd.DataFrame({
-                "doc_type": df_type,
-                "filename": file,
-                "ID": os.path.basename(file),
-                "tokens": [tokens.compute()],  # dask.dataframe somehow interfere with nltk
-                "length": 0},
-                ).astype({"length": "int"})
-            df_part = dd.from_pandas(df_part, chunksize=20)
+            df_part = pd.DataFrame(
+                {
+                    "doc_type": df_type,
+                    "filename": file,
+                    "ID": os.path.basename(file),
+                    "tokens": [
+                        tokens.compute()
+                    ],  # dask.dataframe somehow interfere with nltk
+                    "length": 0,
+                },
+            ).astype({"length": "int"})
+            df_part = dd.from_pandas(df_part, chunksize=self.chunk_size)
             df = dd.concat([df, df_part], interleave_partitions=True)
         df["length"] = df["tokens"].apply(len, meta="int")
         self._update_df(df, df_type)
@@ -85,7 +117,7 @@ class TokenData:
 
     def xls_to_df(self, parent_dir, file_glob="*.xlsx", df_type="train"):
         df = pd.DataFrame(columns=self.df_schema).astype({"length": "int"})
-        df = dd.from_pandas(df, chunksize=20)
+        df = dd.from_pandas(df, chunksize=self.chunk_size)
         for file in glob(os.path.join(parent_dir, file_glob)):
             df_part = dd.from_delayed(dask.delayed(pd.read_excel)(os.path.join(file)))
             df_part["Description"] = df_part["Description"].apply(
@@ -134,10 +166,16 @@ class TokenData:
         if df_type in ["all", "resume"]:
             # encountered bugs here
             # self.res_df.to_parquet(os.path.join(parent_dir, file_path["resume"]), compute=True)
-            self.res_df.compute().to_parquet(join(parent_dir, file_path["resume"]), compression="gzip")
+            self.res_df.compute().to_parquet(
+                join(parent_dir, file_path["resume"]), compression="gzip"
+            )
         if df_type in ["all", "job"]:
             # self.job_df.to_parquet(os.path.join(parent_dir, file_path["job"]), compute=True)
-            self.job_df.compute().to_parquet(join(parent_dir, file_path["job"]), compression="gzip")
+            self.job_df.compute().to_parquet(
+                join(parent_dir, file_path["job"]), compression="gzip"
+            )
         if df_type in ["all", "train"]:
             # self.train_df.to_parquet(os.path.join(parent_dir, file_path["train"]), compute=True)
-            self.train_df.compute().to_parquet(join(parent_dir, file_path["train"]), compression="gzip")
+            self.train_df.compute().to_parquet(
+                join(parent_dir, file_path["train"]), compression="gzip"
+            )
